2aff327fa473bdd9bdb282bde6fdb6c090537711
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2023-08-27 17:22:10 -0300

    Migrations and DbContext

---
+++ Persistence/Migrations/20230827200728_First-Migration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistrationOfBooks.Api.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    release_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    number_pages = table.Column<int>(type: "int", nullable: false),
+                    language = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    publishing_company = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ Persistence/RegistrationBooksDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrationOfBooks.Api.Entities;
+
+namespace RegistrationOfBooks.Api.Persistence
+{
+    public class RegistrationBooksDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasKey(e => e.Id);
+        }
+        public RegistrationBooksDbContext(DbContextOptions<RegistrationBooksDbContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+    }
+}
