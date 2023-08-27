00aa59c9ba6e90e63170eb7aa0e8f6002e12b87a
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2023-08-27 17:24:14 -0300

    folder of entities

---
+++ Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace RegistrationOfBooks.Api.Entities
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string title { get; set; }
+        public string gender { get; set; }
+        public DateTime release_date { get; set; }
+        public string author { get; set; }
+        public string description { get; set; }
+        public int number_pages { get; set; }
+        public string language { get; set; }
+        public string publishing_company { get; set; }
+    }
+}
