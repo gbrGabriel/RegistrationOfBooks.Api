6fdff4b93415a8bdf8431b4f7ef7fef0f8a45c99
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2023-09-27 21:31:51 -0300

    add folder of repository

---
+++ Repository/Repository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrationOfBooks.Api.Entities;
+using RegistrationOfBooks.Api.Interfaces;
+using RegistrationOfBooks.Api.Persistence;
+
+namespace RegistrationOfBooks.Api.Repository
+{
+    public class Repository : IRepository
+    {
+        private readonly RegistrationBooksDbContext _context;
+        public Repository(RegistrationBooksDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Book>> GetAll()
+        {
+            return await _context.Books.AsNoTracking().ToListAsync();
+        }
+
+        public Task<Book> GetById()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
