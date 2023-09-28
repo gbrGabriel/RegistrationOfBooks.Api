2d2d110524bd3e99f4b396901575d7a300e88ad3
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2023-09-27 21:31:36 -0300

    interfaces for repository

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using RegistrationOfBooks.Api.Entities;
+
+namespace RegistrationOfBooks.Api.Interfaces
+{
+    public interface IRepository
+    {
+        Task<List<Book>> GetAll();
+        Task<Book> GetById();
+    }
+}
